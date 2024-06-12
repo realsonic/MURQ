39347b3a74e2565a61460c229f2af2f789419c80
realsonic <realsonic@yandex.ru>
2024-06-12 23:36:45 +0300

    #6: Демо 2: квест Поиске

---
+++ Demo/MURQ.Demo.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+using MURQ.Demo._2;
+using MURQ.Domain.Games;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.Title = "Мурка. Демо 2: Поиске";
+
+var game = new Game(PoiskeQuestBuilder.BuildPoiskeQuest());
+game.Start();
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine(game.CurrentLocation.Text);
+
+    int choicenButtonIndex = ShowButtonsAndGetChoice(game.CurrentLocation.Buttons) - 1;
+
+    game.CurrentLocation.Buttons[choicenButtonIndex].Press();
+}
+
+static int ShowButtonsAndGetChoice(IReadOnlyList<Game.Button> readOnlyList)
+{
+    ShowButtons(readOnlyList);
+    return GetValidChoice(readOnlyList.Count);
+}
+
+static void ShowButtons(IReadOnlyList<Game.Button> buttons)
+{
+    ConsoleColor previousColor = Console.ForegroundColor;
+
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    try
+    {
+        for (var buttonIndex = 0; buttonIndex <= buttons.Count - 1; buttonIndex++)
+        {
+            Game.Button button = buttons[buttonIndex];
+            int buttonNumber = buttonIndex + 1;
+            Console.WriteLine($"[{buttonNumber}] {button.Caption}");
+        }
+    }
+    finally
+    {
+        Console.ForegroundColor = previousColor;
+    }
+}
+
+static int GetValidChoice(int maxNumber)
+{
+    while (true)
+    {
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+        if (!char.IsDigit(keyInfo.KeyChar)) continue;
+
+        var number = Convert.ToInt32(keyInfo.KeyChar.ToString());
+        if (number >= 1 && number <= maxNumber) return number;
+    }
+}
