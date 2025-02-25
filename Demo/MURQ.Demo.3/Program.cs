693f00a7738a54bd5479646a933b6cca14a20e5f
realsonic <realsonic@yandex.ru>
2025-02-26 02:16:14 +0300

    #8: Demo 3

---
+++ Demo/MURQ.Demo.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using MURQ.Application;
+using MURQ.Domain.Games;
+
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.Title = "Мурка. Демо 3";
+
+string questSource = await File.ReadAllTextAsync(@"Demo3.qst");
+var urqLoader = new UrqLoader(questSource);
+var quest = urqLoader.LoadQuest();
+var game = new Game(quest);
+
+game.Start();
+Console.WriteLine(game.CurrentLocation.Text);
+
+Console.Write(">> Нажмите любую клавишу для выхода. <<");
+Console.ReadKey(true);
