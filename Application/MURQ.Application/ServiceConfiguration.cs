875308d34a3743c90ea34731ff3cd3077c482870
realsonic <realsonic@yandex.ru>
2025-08-03 01:14:05 +0300

    Рефакторинг ServiceConfiguration

---
+++ Application/MURQ.Application/ServiceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using MURQ.Application.Interfaces;
+using MURQ.Application.Services;
+using MURQ.Application.UrqLoaders;
+using MURQ.Application.UrqLoaders.UrqStrings;
+
+namespace MURQ.Application;
+public static class ServiceConfiguration
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services) => services
+        .AddTransient<IUrqPlayer, UrqPlayer>()
+        .AddUrqLoaders();
+
+    private static IServiceCollection AddUrqLoaders(this IServiceCollection services) => services
+        .AddTransient<UrqLoader>()
+        .AddTransient<UrqStringLoader>()
+        .AddTransient<UrqStringLexer>();
+}
--- Presentation/MURQ.Console/ServiceConfiguration.cs
+++ Presentation/MURQ.Console/ServiceConfiguration.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
+using MURQ.Application;
 using MURQ.Application.Interfaces;
-using MURQ.Application.Services;
 using MURQ.Application.UrqLoaders;
-using MURQ.Application.UrqLoaders.UrqStrings;
 using MURQ.Infrastructure.ConsoleInterface;
 using MURQ.Infrastructure.QuestLoaders;
 
@@ -11,17 +10,14 @@ namespace MURQ.Console;
 internal static class ServiceConfiguration
 {
     public static ServiceProvider ConfigureServices(string[] args) => new ServiceCollection()
+        .AddApplication()
+        .AddInfrastructure(qstFilePath: args is [string qstFilePath, ..] ? qstFilePath : null)
         .AddTransient<IVersionProvider, MurqConsoleVersionProvider>()
-        .AddTransient<IUrqPlayer, UrqPlayer>()
-        .AddTransient<IUserInterface, ConsoleUserInterface>()
-        .AddUrqLoaders()
-        .AddTransient<IQuestLoader, FileQuestLoader>(serviceProvider => new FileQuestLoader(
-            urqLoader: serviceProvider.GetRequiredService<UrqLoader>(),
-            qstFilePath: args is [string qstFilePath, ..] ? qstFilePath : null))
         .BuildServiceProvider();
 
-    private static IServiceCollection AddUrqLoaders(this IServiceCollection services) => services
-        .AddTransient<UrqLoader>()
-        .AddTransient<UrqStringLoader>()
-        .AddTransient<UrqStringLexer>();
+    private static IServiceCollection AddInfrastructure(this IServiceCollection services, string? qstFilePath) => services
+        .AddTransient<IUserInterface, ConsoleUserInterface>()
+        .AddTransient<IQuestLoader, FileQuestLoader>(serviceProvider => new FileQuestLoader(
+            urqLoader: serviceProvider.GetRequiredService<UrqLoader>(),
+            qstFilePath: qstFilePath));
 }
