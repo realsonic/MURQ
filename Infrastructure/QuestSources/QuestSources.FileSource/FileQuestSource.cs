cf215321e61c2ae24f196664156e72d6d0d8a43a
Mikhail <realsonic@users.noreply.github.com>
2025-09-16 00:02:37 +0300

    #59: рефакторинг загрузки файлов (#102)

---
+++ Infrastructure/QuestSources/QuestSources.FileSource/FileQuestSource.cs
@@ -0,0 +1,26 @@
+﻿using MURQ.Application.Interfaces;
+using MURQ.Application.UrqLoaders;
+using MURQ.Common.Exceptions;
+using MURQ.Domain.Quests;
+
+using System.Text;
+
+namespace QuestSources.FileSource;
+
+public class FileQuestSource(UrqLoader urqLoader, string? filePath, Encoding encoding) : IQuestSource
+{
+    public async Task<(Quest Quest, string SourceName)> GetQuest(CancellationToken stoppingToken)
+    {
+        if (filePath is null)
+            throw new MurqException("Путь к файлу квеста не задан");
+
+        if (!File.Exists(filePath))
+            throw new MurqException($"Заданный файл квеста ({filePath}) не найден (ищу по пути: {Path.GetFullPath(filePath)})");
+
+        string questSource = await File.ReadAllTextAsync(filePath, encoding, stoppingToken);
+
+        Quest quest = urqLoader.LoadQuest(questSource);
+
+        return (quest, $"Файл: {Path.GetFileName(filePath)}");
+    }
+}
