c8d4ea99260bff0117fd2549c15249b9067e02b1
Mikhail <realsonic@users.noreply.github.com>
2025-09-16 18:45:55 +0300

    Флаг кодировки файла (#104)
    * #59: рефакторинг загрузки файлов
    
    * #59: рефакторинг загрузки кодировки
    
    * #59: автоопределение кодировки файла квеста
    
    * Небольшой рефакторинг UrqPlayer
    
    * #59: добавлен параметр командной строки --encoding

--- Infrastructure/MURQ.Infrastructure/ServiceConfiguration.cs
+++ Infrastructure/MURQ.Infrastructure/ServiceConfiguration.cs
@@ -2,6 +2,7 @@
 
 using MURQ.Application.Interfaces;
 using MURQ.Application.UrqLoaders;
+using MURQ.Common.Exceptions;
 using MURQ.Infrastructure.ConsoleInterface;
 
 using QuestSources.FileSource;
@@ -17,12 +18,29 @@ public static class ServiceConfiguration
 
     public static IServiceCollection AddFileQuestSource(this IServiceCollection services, string[] args)
     {
-        string? filePath = args is [string qstFilePath, ..] ? qstFilePath : null;
-
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-        Encoding encoding = Encoding.GetEncoding("Windows-1251");
+        string? filePath = TryGetFilePathArgument(args);
+        Encoding? encoding = TryGetEncodingArgument(args);
 
         return services
-            .AddTransient<IQuestSource, FileQuestSource>(serviceProvider => new FileQuestSource(filePath, serviceProvider.GetRequiredService<UrqLoader>()));
+            .AddTransient<IQuestSource, FileQuestSource>(serviceProvider => new FileQuestSource(serviceProvider.GetRequiredService<UrqLoader>(), filePath, encoding));
+    }
+
+    private static string? TryGetFilePathArgument(string[] args) => args is [string qstFilePath, ..] ? qstFilePath : null;
+
+    private static Encoding? TryGetEncodingArgument(string[] args)
+    {
+        if (args is [_, string paramName, string paramValue] && paramName.ToLower() is "--encoding" or "-enc")
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            return paramValue.ToLower() switch
+            {
+                "utf8" => Encoding.UTF8,
+                "win" => Encoding.GetEncoding("Windows-1251"),
+                "dos" => Encoding.GetEncoding("cp866"),
+                _ => throw new MurqException($"Неизвестная кодировка файла - {paramValue}")
+            };
+        }
+        return null;
     }
 }
--- Infrastructure/QuestSources/QuestSources.FileSource/FileEncodingDetector.cs
+++ Infrastructure/QuestSources/QuestSources.FileSource/FileEncodingDetector.cs
@@ -3,16 +3,17 @@
 namespace QuestSources.FileSource;
 public class FileEncodingDetector
 {
-    public FileEncodingDetector()
+    static FileEncodingDetector()
     {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
     }
 
-    public async ValueTask<Encoding> Detect(string filePath, CancellationToken cancellationToken)
+    public static async ValueTask<Encoding> Detect(string filePath, CancellationToken cancellationToken)
     {
         byte[] buffer = new byte[5];
         await using FileStream fileStream = new(filePath, FileMode.Open);
         await fileStream.ReadAsync(buffer.AsMemory(), cancellationToken);
+
         return DetectEncoding(buffer);
     }
 
@@ -21,7 +22,6 @@ public class FileEncodingDetector
         [0xEF, 0xBB, 0xBF, ..] => Encoding.UTF8,
         [0xFE, 0xFF, ..] => Encoding.Unicode,
         [0, 0, 0xFE, 0xFF, ..] => Encoding.UTF32,
-        [0x2B, 0x2F, 0x76, ..] => Encoding.UTF7,
         [0xFF, 0xFE, ..] => Encoding.GetEncoding(1200), // 1200 utf-16 Unicode
         _ => Encoding.GetEncoding("Windows-1251")
     };
