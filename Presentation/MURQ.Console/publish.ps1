2c847febab5bb174a62eb692e080c41c73f4f6c5
realsonic <realsonic@yandex.ru>
2026-03-27 19:23:07 +0300

    Упаковка win-сборки UPX-ом; в скрипт сборки добавлена версия

--- Presentation/MURQ.Console/publish.ps1
+++ Presentation/MURQ.Console/publish.ps1
@@ -1,16 +1,19 @@
 <# Windows #>
-dotnet publish --os win --configuration Debug --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true -p:PublishAot=true `
+dotnet publish --os win --configuration Debug --self-contained true -p:PublishAot=true `
+&& ($productVersion = (Get-Item .\bin\Debug\net10.0\win-x64\publish\MURQ.Console.exe).VersionInfo.ProductVersionRaw) `
+&& ($version = "$($productVersion.Major).$($productVersion.Minor).$($productVersion.Build)") `
+&& Copy-Item .\bin\Debug\net10.0\win-x64\publish\MURQ.Console.exe .\bin\Debug\net10.0\win-x64\publish\MURQ.Console.$version.exe -Force `
 && [System.Console]::Beep(1000, 700) `
-&& start .\bin\Debug\net9.0\win-x64\publish\ `
+&& start .\bin\Debug\net10.0\win-x64\publish\ `
 <# ---------------------------------------- #>`
 <# Linux #>`
-&& dotnet publish --os linux --configuration Debug --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true `
-&& Copy-Item .\bin\Debug\net9.0\linux-x64\publish\MURQ.Console .\bin\Debug\net9.0\linux-x64\publish\MURQ.Console.Linux -Force `
+&& dotnet publish --os linux --configuration Debug --self-contained true -p:StripSymbols=true `
+&& Copy-Item .\bin\Debug\net10.0\linux-x64\publish\MURQ.Console .\bin\Debug\net10.0\linux-x64\publish\MURQ.Console.Linux.$version -Force `
 && [System.Console]::Beep(2000, 700) `
-&& start .\bin\Debug\net9.0\linux-x64\publish\ `
+&& start .\bin\Debug\net10.0\linux-x64\publish\ `
 <# ---------------------------------------- #>`
-<# Mac #>`
-&& dotnet publish --os osx --configuration Debug --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true `
-&& Copy-Item .\bin\Debug\net9.0\osx-x64\publish\MURQ.Console .\bin\Debug\net9.0\osx-x64\publish\MURQ.Console.OSX -Force `
+<# OS X #>`
+&& dotnet publish --os osx --configuration Debug --self-contained true -p:StripSymbols=true `
+&& Copy-Item .\bin\Debug\net10.0\osx-x64\publish\MURQ.Console .\bin\Debug\net10.0\osx-x64\publish\MURQ.Console.OSX.$version -Force `
 && [System.Console]::Beep(3000, 700) `
-&& start .\bin\Debug\net9.0\osx-x64\publish\ `
+&& start .\bin\Debug\net10.0\osx-x64\publish\ `
