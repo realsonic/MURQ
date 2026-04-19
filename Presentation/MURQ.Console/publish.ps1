3c781eb6f501cfe8039f53dca0bb280e59867f76
realsonic <realsonic@yandex.ru>
2026-04-19 15:57:41 +0300

    Исправлена ошибка версии при публикации; VersionSuffix в проекте теперь передаётся через переменную окружения VersionSuffix

--- Presentation/MURQ.Console/publish.ps1
+++ Presentation/MURQ.Console/publish.ps1
@@ -1,17 +1,18 @@
 param (
+	[string]$VersionSuffix,
 	[switch]$AllPlatforms
 )
 
 $ErrorActionPreference = 'Stop'
 
 <# Windows #>
-dotnet publish --os win --configuration Debug --self-contained true -p:PublishAot=true
+dotnet publish --os win --configuration Debug --self-contained true -p:VersionSuffix=$VersionSuffix -p:PublishAot=true
 if ($LastExitCode -ne 0) { throw "Внешняя программа завершилась с ошибкой (код: $LastExitCode)" }
 $versionInfo = (Get-Item .\bin\Debug\net10.0\win-x64\publish\MURQ.Console.exe).VersionInfo
-$productVersion = $versionInfo.ProductVersionRaw
+$fileVersion = $versionInfo.FileVersionRaw
 $fullVersion = $versionInfo.ProductVersion
 Write-Output "Версия: $fullVersion"
-$version = "$($productVersion.Major).$($productVersion.Minor).$($productVersion.Build)"
+$version = "$($fileVersion.Major).$($fileVersion.Minor).$($fileVersion.Build)"
 Copy-Item .\bin\Debug\net10.0\win-x64\publish\MURQ.Console.exe .\bin\Debug\net10.0\win-x64\publish\MURQ.Console.$version.exe -Force
 Copy-Item .\bin\Debug\net10.0\win-x64\publish\MURQ.Console.exe .\bin\Debug\net10.0\win-x64\publish\MURQ.Console.$fullVersion.exe -Force
 [System.Console]::Beep(1000, 700)
@@ -20,7 +21,7 @@ start .\bin\Debug\net10.0\win-x64\publish\ -WindowStyle Minimized
 if (!$AllPlatforms) { return }
 
 <# Linux #>
-dotnet publish --os linux --configuration Debug --self-contained true -p:StripSymbols=true
+dotnet publish --os linux --configuration Debug --self-contained true -p:VersionSuffix=$VersionSuffix -p:StripSymbols=true
 if ($LastExitCode -ne 0) { throw "Внешняя программа завершилась с ошибкой (код: $LastExitCode)" }
 Copy-Item .\bin\Debug\net10.0\linux-x64\publish\MURQ.Console .\bin\Debug\net10.0\linux-x64\publish\MURQ.Console.Linux.$version -Force
 #Copy-Item .\bin\Debug\net10.0\linux-x64\publish\MURQ.Console .\bin\Debug\net10.0\linux-x64\publish\MURQ.Console.Linux.$fullVersion -Force
@@ -28,7 +29,7 @@ Copy-Item .\bin\Debug\net10.0\linux-x64\publish\MURQ.Console .\bin\Debug\net10.0
 start .\bin\Debug\net10.0\linux-x64\publish\ -WindowStyle Minimized
 
 <# OS X #>`
-dotnet publish --os osx --configuration Debug --self-contained true -p:StripSymbols=true
+dotnet publish --os osx --configuration Debug --self-contained true -p:VersionSuffix=$VersionSuffix -p:StripSymbols=true
 if ($LastExitCode -ne 0) { throw "Внешняя программа завершилась с ошибкой (код: $LastExitCode)" }
 Copy-Item .\bin\Debug\net10.0\osx-x64\publish\MURQ.Console .\bin\Debug\net10.0\osx-x64\publish\MURQ.Console.OSX.$version -Force
 #Copy-Item .\bin\Debug\net10.0\osx-x64\publish\MURQ.Console .\bin\Debug\net10.0\osx-x64\publish\MURQ.Console.OSX.$fullVersion -Force
