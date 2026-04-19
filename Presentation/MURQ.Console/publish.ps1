8d1388b3d6c6a7345807e24c77f84bd7a51defc8
realsonic <realsonic@yandex.ru>
2026-04-19 16:25:58 +0300

    Добавлены подсказки к параметрам при публикации проекта

--- Presentation/MURQ.Console/publish.ps1
+++ Presentation/MURQ.Console/publish.ps1
@@ -1,5 +1,8 @@
 param (
+	[Parameter(HelpMessage="(греческие буквы для версий: alpha, beta, gamma, delta, epsilon, zeta, eta, theta, iota, kappa, lambda, mu, nu, xi, omicron, pi, rho, sigma, tau, upsilon, phi, chi, psi, omega)")]
 	[string]$VersionSuffix,
+	
+	[Parameter(HelpMessage="Собрать для всех платформ")]
 	[switch]$AllPlatforms
 )
 
