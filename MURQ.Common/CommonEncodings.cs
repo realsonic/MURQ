aca30f3561961a6244e4856f49019a582805b1db
realsonic <realsonic@yandex.ru>
2026-05-03 00:15:26 +0300

    #109 FileQuestSource переведён на StreamReader с буфером 8 Кб и встроенным автоопределителем кодировок по преамбуле (параметр detectEncodingFromByteOrderMarks).

---
+++ MURQ.Common/CommonEncodings.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace MURQ.Common;
+
+public static class CommonEncodings
+{
+    static CommonEncodings()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    }
+
+    public static Encoding UTF8 => Encoding.UTF8;
+    public static Encoding Windows => Encoding.GetEncoding("windows-1251");
+    public static Encoding DOS => Encoding.GetEncoding("cp866");
+}
