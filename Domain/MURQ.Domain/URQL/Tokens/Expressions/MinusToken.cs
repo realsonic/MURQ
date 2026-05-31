92343d30a035e2b188af58c00695efc60bf68e4f
realsonic <realsonic@yandex.ru>
2026-05-31 22:47:06 +0300

    #145 Добавлены токены для арифметических операций

---
+++ Domain/MURQ.Domain/URQL/Tokens/Expressions/MinusToken.cs
@@ -0,0 +1,8 @@
+﻿using MURQ.Domain.URQL.Locations;
+
+using System.ComponentModel;
+
+namespace MURQ.Domain.URQL.Tokens.Expressions;
+
+[Description(@"минус ""-""")]
+public record MinusToken(string Lexeme, Location Location) : Token(Lexeme, Location);
