102bb0bb906f19933645a495b61ca8753bc1e07a
realsonic <realsonic@yandex.ru>
2026-06-24 00:27:55 +0300

    feat(#146): добавлены унарные операции + и -

---
+++ Domain/MURQ.Domain/Quests/Expressions/UnaryMinusExpression.cs
@@ -0,0 +1,24 @@
+﻿using MURQ.Domain.Games;
+using MURQ.Domain.Games.Values;
+
+using System.Diagnostics;
+
+namespace MURQ.Domain.Quests.Expressions;
+
+[DebuggerDisplay("-{Expression}")]
+public class UnaryMinusExpression : Expression
+{
+    public required Expression Expression { get; init; }
+
+    public override Value Calculate(IGameContext gameContext)
+    {
+        Value value = Expression.Calculate(gameContext);
+
+        return value switch
+        {
+            NumberValue numberValue => new NumberValue(-numberValue.Value),
+            StringValue stringValue => new NumberValue(-stringValue.Value.Length),
+            _ => throw new NotImplementedException($"Тип значения {value.GetType()} ещё не обрабытвается.")
+        };
+    }
+}
---
+++ Domain/MURQ.Domain/Quests/Expressions/UnaryPlusExpression.cs
@@ -0,0 +1,18 @@
+﻿using MURQ.Domain.Games;
+using MURQ.Domain.Games.Values;
+
+using System.Diagnostics;
+
+namespace MURQ.Domain.Quests.Expressions;
+
+[DebuggerDisplay("+{Expression}")]
+public class UnaryPlusExpression : Expression
+{
+    public required Expression Expression { get; init; }
+
+    public override Value Calculate(IGameContext gameContext)
+    {
+        Value value = Expression.Calculate(gameContext);
+        return value;
+    }
+}
